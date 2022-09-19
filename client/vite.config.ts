e9935e0c5a07a084ef8e1cac05110d32532011d8
Kirk Morris <worker.kirk@gmail.com>
2022-09-19 12:28:29 -0700

    Updates vite config to pass through COORDINATOR_HOST.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,12 +5,18 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, "../", "");
   const appSecret = process.env.APP_SECRET ?? env.APP_SECRET;
   process.env.APP_ID = hash.sha256().update(appSecret).digest("hex");
+  
   return {
     build: { target: "esnext" },
-    envDir: "../",
-    envPrefix: "APP_ID",
     publicDir: "src/assets",
     server: { host: "0.0.0.0" },
     clearScreen: false,
+    define: {
+      "process.env": {
+        APP_ID: process.env.APP_ID,
+        COORDINATOR_HOST: env.COORDINATOR_HOST,
+        MATCHMAKER_HOST: env.MATCHMAKER_HOST,
+      }
+    }
   };
 });
